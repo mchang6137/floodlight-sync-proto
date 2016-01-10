5040fda7cea66c28b3f918edeae3bbe43132f7b7
michael chang <machang@princeton.edu>
2016-01-10 16:25:52 -0500

    Mininet topology

--- internet2.py
+++ internet2.py
@@ -12,15 +12,14 @@ CORES = {
   'S3': {'dpid': '000000000000030%s'},
   'S4': {'dpid': '000000000000040%s'},
   'S5': {'dpid': '000000000000050%s'},
-  'S6': {'dpid': '000000000000060%s'},
   }
 
-FANOUT = 4
+FANOUT = 1
     
 class I2Topo(Topo):
 
   def __init__(self, enable_all = True):
-    "Create Internet2 topology."
+    "Create Simple policy"
 
     # Add default members to class.
     super(I2Topo, self).__init__()
@@ -30,26 +29,28 @@ class I2Topo(Topo):
     for switch in CORES:
       self.cores[switch] = self.addSwitch(switch, dpid=(CORES[switch]['dpid'] % '0'))
 
+    switchcount = 0
+
     # Add hosts and connect them to their core switch
     for switch in CORES:
       for count in xrange(1, FANOUT + 1):
         # Add hosts
         host = 'h_%s_%s' % (switch, count)
-        ip = '10.0.0.%s' % count
+        print host
+        ip = '10.0.0.%s' % (count + switchcount)
+        print ip
         mac = CORES[switch]['dpid'][4:] % count
         h = self.addHost(host, ip=ip, mac=mac)
         # Connect hosts to core switches
         self.addLink(h, self.cores[switch])
+        switchcount = switchcount + 1
 
     # Connect core switches
     self.addLink(self.cores['S1'], self.cores['S2'])
+    self.addLink(self.cores['S2'], self.cores['S5'])
     self.addLink(self.cores['S1'], self.cores['S3'])
-    self.addLink(self.cores['S2'], self.cores['S3'])
-    self.addLink(self.cores['S2'], self.cores['S4'])
-    self.addLink(self.cores['S3'], self.cores['S2'])
+    self.addLink(self.cores['S3'], self.cores['S4'])
     self.addLink(self.cores['S4'], self.cores['S5'])
-    self.addLink(self.cores['S6'], self.cores['S4'])
-    self.addLink(self.cores['S5'], self.cores['S6'])
 
 
 if __name__ == '__main__':
